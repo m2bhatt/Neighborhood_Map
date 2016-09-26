25b99a35da7fad2b128b8760a2a5614a2bc39c3b
Megha Bhatt <megha.bhatt@shopify.com>
2016-09-26 17:05:50 -0400

    Changes

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,5 @@
+/* Main js file for the Neighborhood Map.
+Gets the Google Map */
 var map;
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
@@ -7,15 +9,59 @@ function initMap() {
   });
 }
 
+var initialStops = [
+  {
+    stop_num: '7688',
+    stop_name: 'Bank / Slater',
+    stop_lat: '45.419109',
+    stop_lon: '-75.699486'
+  },
+  {
+    stop_num: '3006',
+    stop_name: 'Slater / Kent',
+    stop_lat: '45.4179',
+    stop_lon: '-75.70282'
+  },
+  {
+    stop_num: '7694',
+    stop_name: 'Bank/Slater',
+    stop_lat: '45.419109',
+    stop_lon: '-75.699486'
+  },
+  {
+    stop_num: '7688',
+    stop_name: 'Bank/Slater',
+    stop_lat: '45.419109',
+    stop_lon: '-75.699486'
+  },
+  {
+    stop_num: '7688',
+    stop_name: 'Bank/Slater',
+    stop_lat: '45.419109',
+    stop_lon: '-75.699486'
+  }
+]
 
+var currentStop = function(data){
+  this.stop_num = ko.observable(data.stop_num);
+  this.stop_name = ko.observable(data.stop_name);
+  this.stop_lat = ko.observable(data.stop_lat);
+  this.stop_lon = ko.observable(data.stop_lon);
+}
 /* VIEW MODEL */
 
-var ViewModel = function (){
-  this.stop_id = ko.observable();
-  this.stop_num = ko.observable();
-  this.stop_name = ko.observable();
-  this.stop_lat = ko.observable();
-  this.stop_lon = ko.observable();
-}
+// Make stops show up in a list in HTML
+function stopViewModel () {
+  var self = this;
 
-ko.applyBindings(new ViewModel())
+  this.stopList = ko.observableArray([]);
+
+  initialStops.forEach(function(stopItem){
+    self.stopList.push(new currentStop(stopItem));
+  });
+
+  this.currentStop = ko.observable(this.stopList()[0]);
+
+  };
+
+ko.applyBindings(new stopViewModel());
