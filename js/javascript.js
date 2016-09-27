b01ce499b068fd27ff53f225d3b312dea18dd136
Megha Bhatt <megha.bhatt@shopify.com>
2016-09-27 13:49:39 -0400

    Added markers

--- js/javascript.js
+++ js/javascript.js
@@ -1,67 +1,128 @@
 /* Main js file for the Neighborhood Map.
 Gets the Google Map */
 var map;
+
+var markers = [];
+
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 45.4215, lng: -75.6972},
-    zoom: 15
+    zoom: 16
   });
+  /* var tribeca = {lat: 45.419109, lng: -75.699486};
+  var marker = new google.maps.Marker({
+    position: tribeca,
+    map: map,
+    title: 'Bank /Slater'
+  });
+  var infoWindow = new google.maps.InfoWindow({
+    content: 'Routes 1: Ottawa-Rockcliffe'
+  });
+  marker.addListener('click', function(){
+    infoWindow.open(map, marker);
+  }); */
+
+
+var locations = [
+  {title: 'Bank / Slater', location: {lat: 45.419109, lng: -75.699486}},
+  {title: 'Slater / Kent', location: {lat: 45.4179, lng: -75.70282}},
+  {title: 'Wellington / Metacalfe (Parliament)', location: {lat: 45.423405, lng: -75.698051}},
+  {title: 'Queen / Bank', location: {lat:45.420593, lng: -75.700485}},
+  {title: 'Queen / Metcalfe', location: {lat:45.421982, lng:-75.697273}}
+];
+
+var largeInfowindow = new google.maps.InfoWindow();
+var bounds = new google.maps.LatLngBounds();
+
+//The following group uses the location array to create an array of markers on intialize.
+for (var i = 0; i < locations.length; i++) {
+  var position = locations[i].location;
+  var title = locations[i].title;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: position,
+    title: title,
+    animation: google.maps.Animation.DROP,
+    id: i
+  });
+
+  markers.push(marker);
+
+  marker.addListener('click', function(){
+    populateInfoWindow(this, largeInfowindow);
+  });
+  bounds.extend(markers[i].position);
 }
+map.fitBounds(bounds);
+}
+
+function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.open(map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick',function(){
+            infowindow.setMarker(null);
+          });
+        }
+      }
 
 var initialStops = [
   {
-    stop_num: '7688',
-    stop_name: 'Bank / Slater',
-    stop_lat: '45.419109',
-    stop_lon: '-75.699486'
+    stopNum: '7688',
+    stopName: 'Bank / Slater',
+    stopLat: '45.419109',
+    stopLon: '-75.699486'
   },
   {
-    stop_num: '3006',
-    stop_name: 'Slater / Kent',
-    stop_lat: '45.4179',
-    stop_lon: '-75.70282'
+    stopNum: '3006',
+    stopName: 'Slater / Kent',
+    stopLat: '45.4179',
+    stopLon: '-75.70282'
   },
   {
-    stop_num: '7694',
-    stop_name: 'Bank/Slater',
-    stop_lat: '45.419109',
-    stop_lon: '-75.699486'
+    stopNum: '7694',
+    stopName: 'Wellington / Metacalfe (Parliament)',
+    stopLat: '45.423405',
+    stopLon: '-75.698051'
   },
   {
-    stop_num: '7688',
-    stop_name: 'Bank/Slater',
-    stop_lat: '45.419109',
-    stop_lon: '-75.699486'
+    stopNum: '7561',
+    stopName: 'Queen / Bank',
+    stopLat: '45.420593',
+    stopLon: '-75.700485'
   },
   {
-    stop_num: '7688',
-    stop_name: 'Bank/Slater',
-    stop_lat: '45.419109',
-    stop_lon: '-75.699486'
+    stopNum: '1512',
+    stopName: 'Queen / Metcalfe',
+    stopLat: '45.421982',
+    stopLon: '-75.697273'
   }
-]
+];
 
-var currentStop = function(data){
-  this.stop_num = ko.observable(data.stop_num);
-  this.stop_name = ko.observable(data.stop_name);
-  this.stop_lat = ko.observable(data.stop_lat);
-  this.stop_lon = ko.observable(data.stop_lon);
-}
+var Stop = function(data){
+  this.stopNum = ko.observable(data.stopNum);
+  this.stopName = ko.observable(data.stopName);
+  this.stopLat = ko.observable(data.stopLat);
+  this.stopLon = ko.observable(data.stopLon);
+};
 /* VIEW MODEL */
 
 // Make stops show up in a list in HTML
-function stopViewModel () {
+var ViewModel = function() {
   var self = this;
 
   this.stopList = ko.observableArray([]);
 
   initialStops.forEach(function(stopItem){
-    self.stopList.push(new currentStop(stopItem));
+    self.stopList.push(new Stop(stopItem));
   });
 
   this.currentStop = ko.observable(this.stopList()[0]);
 
   };
 
-ko.applyBindings(new stopViewModel());
+ko.applyBindings(new ViewModel());
